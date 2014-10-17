86b56abf7ed34e8bda8f8462dba102bf958d3b49
Michael Maurizi <mmaurizi@azavea.com>
2014-10-17 15:47:50 -0400

    Add bundle minification + sourcemaps

--- js_proof_of_concept/gulpfile.js
+++ js_proof_of_concept/gulpfile.js
@@ -3,26 +3,47 @@ var gulp = require('gulp'),
     browserify = require('browserify'),
     factor = require('factor-bundle'),
     fs = require('fs'),
-    path = require('path');
+    through = require('through2'),
+    merge = require('merge-stream'),
+    buffer = require('vinyl-buffer'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps');
 
 var entries = ['home.js', 'user.js'],
     entryFiles = entries.map(function(file) { return './js/' + file; }),
-    bundleDir = 'bundle/',
-    entryBundles = entries.map(function(file) { return path.resolve(bundleDir + file); });
+    bundleDir = 'bundle/';
 
 gulp.task('browserify', function() {
-    console.log(entryFiles);
-    console.log(entryBundles);
-    return browserify({
-        entries: entryFiles
+    // We need to use a through-stream for entry bundles so we can minify them
+    var bundleStreams = entries.map(function() { return through(); });
+
+    var commonBundle = browserify({
+        entries: entryFiles,
+        debug: true
     })
     .plugin(factor, {
         entries: entryFiles,
-        o: entryBundles
+        o: bundleStreams
     })
     .bundle()
-    .pipe(source('common.js'))
-    .pipe(gulp.dest(bundleDir));
+    .pipe(source('common.js'));
+
+    var entryBundles = bundleStreams.map(function(stream, i) {
+        return bundleStreams[i].pipe(source(entries[i]));
+    });
+
+    var bundles = entryBundles.concat(commonBundle);
+
+    bundles = bundles.map(function(bundle) {
+        return bundle
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(uglify())
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest(bundleDir));
+    });
+
+    return merge.apply(this, bundles);
 });
 
 gulp.task('default', ['browserify']);
