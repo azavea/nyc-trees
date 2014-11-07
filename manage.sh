76a1546f2a0ec1ae35b814029fff2a13575dbd55
Michael Maurizi <mmaurizi@azavea.com>
2014-11-07 16:21:27 -0500

    Use sane defaults for the Django runserver
    If you don't use 0.0.0.0:8000, the runserver is only accessible from
    the VM itself.

--- manage.sh
+++ manage.sh
@@ -3,4 +3,11 @@
 set -e
 set -x
 
-vagrant ssh app -c "envdir /etc/nyc-trees.d/env /opt/app/manage.py $*"
+ARGS=$*
+
+# Sane defaults for runserver
+if [[ $# -eq 1 ]] && [[ $1 == "runserver" ]]; then
+    ARGS="runserver 0.0.0.0:8000"
+fi
+
+vagrant ssh app -c "envdir /etc/nyc-trees.d/env /opt/app/manage.py $ARGS"
