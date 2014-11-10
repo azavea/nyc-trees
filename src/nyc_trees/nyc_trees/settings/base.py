de44aeefea84273fb596b7bbbafe21f8c9f95a24
Michael Maurizi <mmaurizi@azavea.com>
2014-11-10 13:02:35 -0500

    Change collected static directory to a more obvious name

--- src/nyc_trees/nyc_trees/settings/base.py
+++ src/nyc_trees/nyc_trees/settings/base.py
@@ -79,7 +79,7 @@ MEDIA_URL = '/media/'
 
 # STATIC FILE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = normpath(join(SITE_ROOT, 'assets'))
+STATIC_ROOT = normpath(join(SITE_ROOT, 'collected_static'))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
