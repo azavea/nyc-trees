61bcd99f49ba4d86159b69704aa268796fa02abc
Steve Lamb <slamb@azavea.com>
2014-11-11 15:35:16 -0500

    Add proof of concept script for gunicorn debugging

---
+++ debugserver.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+set -x
+
+vagrant ssh app -c "sudo service nyc-trees-app stop || /bin/true"
+vagrant ssh app -c "cd /opt/app/ && envdir /etc/nyc-trees.d/env gunicorn --config /etc/nyc-trees.d/gunicorn.py nyc_trees.wsgi"
