b4a27d75f8bb0d1191f659803d022356dbba7ce1
Michael Maurizi <mmaurizi@azavea.com>
2014-11-11 17:13:51 -0500

    Remove lint

--- src/nyc_trees/nyc_trees/settings/development.py
+++ src/nyc_trees/nyc_trees/settings/development.py
@@ -1,10 +1,6 @@
 """Development settings and globals."""
 
-
-from os import environ
-from os.path import join, normpath
-
-from base import *
+from base import *  # NOQA
 
 
 # DEBUG CONFIGURATION
@@ -33,22 +29,22 @@ CACHES = {
 
 
 # TOOLBAR CONFIGURATION
-# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+# See https://github.com/django-debug-toolbar/django-debug-toolbar#installation
 INSTALLED_APPS += (
     'debug_toolbar',
 )
 
-# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
-INTERNAL_IPS = ('127.0.0.1',)
+# See https://github.com/django-debug-toolbar/django-debug-toolbar#installation
 
-# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+# See https://github.com/django-debug-toolbar/django-debug-toolbar#installation
 MIDDLEWARE_CLASSES += (
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
-# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+# See https://github.com/django-debug-toolbar/django-debug-toolbar#installation
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
     'SHOW_TEMPLATE_CONTEXT': True,
 }
+
 # END TOOLBAR CONFIGURATION
