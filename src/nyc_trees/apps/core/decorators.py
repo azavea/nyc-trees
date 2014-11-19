8f16784809c01ab64f475d7b6d4c358e8e421826
Rick Mohr <rmohr@azavea.com>
2014-11-19 10:59:56 -0500

    User profile page basics

--- src/nyc_trees/apps/core/decorators.py
+++ src/nyc_trees/apps/core/decorators.py
@@ -3,8 +3,28 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 
-from django.http import Http404
 from functools import wraps
+
+from django.http import Http404
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def decorate(*reversed_views):
+    """
+    provide a syntax decorating views without nested calls.
+
+    instead of:
+    instance_request(json_api_call(etag(<hash_fn>)(<view_fn>)))
+
+    you can write:
+    decorate(instance_request, json_api_call, etag(<hash_fn>), <view_fn>)
+    """
+    fns = reversed_views[::-1]
+    view = fns[0]
+    for wrapper in fns[1:]:
+        view = wrapper(view)
+    return view
 
 
 def route(**kwargs):
@@ -26,6 +46,44 @@ def route(**kwargs):
         else:
             raise Http404()
     return routed
+
+
+def render_template(template):
+    """
+    takes a template to render to and returns a function that
+    takes an object to render the data for this template.
+
+    If callable_or_dict is callable, it will be called with
+    the request and any additional arguments to produce the
+    template paramaters. This is useful for a view-like function
+    that returns a dict-like object instead of an HttpResponse.
+
+    Otherwise, callable_or_dict is used as the parameters for
+    the rendered response.
+    """
+    def outer_wrapper(callable_or_dict=None, statuscode=None, **kwargs):
+        def wrapper(request, *args, **wrapper_kwargs):
+            if callable(callable_or_dict):
+                params = callable_or_dict(request, *args, **wrapper_kwargs)
+            else:
+                params = callable_or_dict
+
+            # If we want to return some other response
+            # type we can, that simply overrides the default
+            # behavior
+            if params is None or isinstance(params, dict):
+                resp = render_to_response(template, params,
+                                          RequestContext(request), **kwargs)
+            else:
+                resp = params
+
+            if statuscode:
+                resp.status_code = statuscode
+
+            return resp
+
+        return wrapper
+    return outer_wrapper
 
 
 def is_logged_in(view_fn):
--- src/nyc_trees/apps/users/views/user.py
+++ src/nyc_trees/apps/users/views/user.py
@@ -3,10 +3,32 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 
-def user_detail(request, username):
-    # TODO: implement
-    pass
+from apps.core.decorators import decorate as do, render_template
+from apps.core.models import User
+
+
+def user_profile_context(request, username):
+    user = get_object_or_404(User, username=username)
+    its_me = (user.id == request.user.id)
+
+    if not its_me and not user.profile_is_public:
+        # Private profile acts like a missing page to others
+        raise Http404()
+
+    return {
+        'user': user,
+        'viewing_own_profile': its_me,
+        'show_username': ((its_me or user.real_name_is_public) and
+                          (user.first_name or user.last_name)),
+        'show_achievements': its_me or user.achievements_are_public,
+        'show_contributions': its_me or user.contributions_are_public,
+        'show_groups': its_me or user.group_follows_are_public,
+        'show_individual_mapper': (user.individual_mapper and
+                                   (its_me or user.profile_is_public))
+    }
 
 
 def update_user(request, username):
@@ -32,3 +54,8 @@ def start_map_for_reservation_job(request, username):
 def start_map_for_tool_depots_job(request, username):
     # TODO: implement
     pass
+
+
+user_detail = do(
+    render_template('users/profile.html'),
+    user_profile_context)
