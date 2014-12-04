7cada33b4af52d7dc619a641556a8514df3548c9
Justin Walgran <jwalgran@azavea.com>
2014-12-04 11:02:52 -0700

    Add a form to edit group settings
    I followed an often used convention of using /model/{id}/edit/ as the
    url for displaying the form. The form POSTs back to /model/{id}/edit/
    and then redirects to /model/{id}/ on success.

--- src/nyc_trees/apps/users/views/group.py
+++ src/nyc_trees/apps/users/views/group.py
@@ -3,11 +3,14 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 
 from django_tinsel.decorators import render_template
 
 from apps.core.models import Group
+from apps.users.forms import GroupSettingsForm
 
 
 def group_list_page(request):
@@ -23,14 +26,39 @@ def group_detail(request, group_slug):
 @render_template('groups/detail.html')
 def _render_group_detail(request, group):
     context = {
-        'group': group
+        'group': group,
+        # TODO: check if user is group admin or census admin
+        'user_can_edit_group': True,
+        'edit_url': reverse('group_edit', kwargs={'group_slug': group.slug})
     }
     return context
 
 
+@render_template('groups/settings.html')
 def edit_group(request, group_slug):
-    # TODO: implement
-    pass
+    group = get_object_or_404(Group, slug=group_slug)
+    form = GroupSettingsForm(instance=group, label_suffix='')
+    context = {
+        'form': form,
+        'group_slug': group_slug
+    }
+    return context
+
+
+@render_template('groups/settings.html')
+def update_group_settings(request, group_slug):
+    group = get_object_or_404(Group, slug=group_slug)
+    form = GroupSettingsForm(request.POST, instance=group)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect(
+            reverse('group_detail', kwargs={'group_slug': group_slug}))
+    else:
+        context = {
+            'form': form,
+            'group_slug': group_slug
+        }
+        return context
 
 
 def follow_group(request, group_slug):
