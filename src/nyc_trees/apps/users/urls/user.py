153de6e53c0ad56d33cfb5796df59592a249fe11
Steve Lamb <slamb@azavea.com>
2014-12-12 18:00:24 -0500

    Use routes pattern in users app

--- src/nyc_trees/apps/users/urls/user.py
+++ src/nyc_trees/apps/users/urls/user.py
@@ -4,53 +4,34 @@ from __future__ import unicode_literals
 from __future__ import division
 
 from django.conf.urls import patterns, url
-from django.contrib.auth.decorators import login_required
-
-from django_tinsel.decorators import route
-
-from apps.users.views.user import (user_detail, achievements_page,
-                                   request_individual_mapper_status,
-                                   start_form_for_reservation_job,
-                                   start_map_for_reservation_job,
-                                   start_map_for_tool_depots_job,
-                                   user_detail_redirect, profile_settings,
-                                   update_profile_settings, training_page)
-
+from apps.users.routes import user as r
 
 # These URLs have the prefix 'user/'
 urlpatterns = patterns(
     '',
-    url(r'^profile/$', login_required(route(GET=user_detail_redirect)),
-        name='user_detail_redirect'),
+    url(r'^profile/$', r.user_detail_redirect, name='user_detail_redirect'),
 
-    url(r'settings/$',
-        login_required(route(GET=profile_settings,
-                             POST=update_profile_settings)),
-        name='user_profile_settings'),
+    url(r'settings/$', r.user_profile_settings, name='user_profile_settings'),
 
-    url(r'^achievements/$', login_required(route(GET=achievements_page)),
-        name='achievements'),
+    url(r'^achievements/$', r.achievements, name='achievements'),
 
-    url(r'^training/$', login_required(route(GET=training_page)),
-        name='training'),
+    url(r'^training/$', r.training, name='training'),
 
-    url(r'^(?P<username>\w+)/$',
-        route(GET=user_detail),
-        name='user_detail'),
+    url(r'^(?P<username>\w+)/$', r.user_detail, name='user_detail'),
 
     url(r'^(?P<username>\w+)/request-individual-mapper-status/$',
-        login_required(route(POST=request_individual_mapper_status)),
+        r.request_individual_mapper_status,
         name='request_individual_mapper_status'),
 
     url(r'^(?P<username>\w+)/printable-survey-form/$',
-        route(POST=start_form_for_reservation_job),
+        r.start_form_for_reservation_job,
         name='start_form_for_reservation_job'),
 
     url(r'^(?P<username>\w+)/printable-map/$',
-        route(POST=start_map_for_reservation_job),
+        r.start_map_for_reservation_job,
         name='start_map_for_reservation_job'),
 
     url(r'^(?P<username>\w+)/printable-tooldepots/$',
-        route(POST=start_map_for_tool_depots_job),
+        r.start_map_for_tool_depots_job,
         name='start_map_for_tool_depots_job'),
 )
