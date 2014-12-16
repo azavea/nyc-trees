062b386af062075dc7b3e9be520a48bc41897772
Michael Maurizi <mmaurizi@azavea.com>
2014-12-16 17:04:57 -0500

    Add "Use my contact info" link to add event page

--- src/nyc_trees/js/src/add_event.js
+++ src/nyc_trees/js/src/add_event.js
@@ -5,7 +5,25 @@ require('bootstrap');
 var Modernizr = require('modernizr'),
     $ = require('jquery');
 
+var dom = {
+    useMyInfo: '#use-my-contact-info',
+    userContactInfo: '#user-contact-info',
+    userContactEmail: '#user-contact-email',
+    eventContactInfo: '#event-form [name="contact_info"]',
+    eventContactEmail: '#event-form [name="contact_email"]'
+};
+
 Modernizr.load({
     test: Modernizr.inputtypes.date && Modernizr.inputtypes.time,
     nope: config.files["datetimepicker_polyfill.js"]
+});
+
+$(dom.useMyInfo).on('click', function(e) {
+    e.preventDefault();
+
+    var userEmail = $(dom.userContactEmail).val(),
+        userInfo = $(dom.userContactInfo).val();
+
+    $(dom.eventContactEmail).val(userEmail);
+    $(dom.eventContactInfo).val(userInfo);
 });
