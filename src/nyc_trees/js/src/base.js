d4a913af099cec55cca41df5ea8536f40d67d24f
Justin Walgran <jwalgran@azavea.com>
2014-12-18 18:13:48 -0700

    Add CSRF protection for AJAX calls
    Without this setup, $.ajax POSTs and DELETEs fail.

--- src/nyc_trees/js/src/base.js
+++ src/nyc_trees/js/src/base.js
@@ -1,4 +1,9 @@
 "use strict";
 
+var $ = require('jquery'),
+    csrf = require('./csrf');
+
 // Place JS here if it needs to be executed on every page.
 require('bootstrap');
+
+$.ajaxSetup(csrf.jqueryAjaxSetupOptions);
