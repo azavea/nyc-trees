7a241f317ec077dafa33ddb9a4f257180c7fb313
Kevin DeLoach <kdeloach@azavea.com>
2014-12-22 12:05:50 -0500

    Add registration UI tests
    This adds coverage for TOS and password validation and makes sure the
    user is_minor flag is correct based on input from the registration form.

--- src/nyc_trees/apps/login/uitests.py
+++ src/nyc_trees/apps/login/uitests.py
@@ -35,6 +35,31 @@ class FullRegistrationUITest(NycTreesSeleniumTestCase):
         user.save()
         return user
 
+    def test_tos_validation(self):
+        self.get('/accounts/register/')
+
+        self.wait_for_textbox_then_type('[name="username"]', 'test')
+        self.wait_for_textbox_then_type('[name="email"]', 'test@aol.com')
+        self.wait_for_textbox_then_type('[name="password1"]', 'banana')
+        self.wait_for_textbox_then_type('[name="password2"]', 'banana')
+
+        self.click('form input[type="submit"]')
+
+        self.wait_for_text("You must agree to the terms to register")
+
+    def test_password_validation(self):
+        self.get('/accounts/register/')
+
+        self.wait_for_textbox_then_type('[name="username"]', 'test')
+        self.wait_for_textbox_then_type('[name="email"]', 'test@aol.com')
+        self.wait_for_textbox_then_type('[name="password1"]', 'banana')
+        self.wait_for_textbox_then_type('[name="password2"]', 'apple')
+        self.click('[name="tos"]')
+
+        self.click('form input[type="submit"]')
+
+        self.wait_for_text("The two password fields didn't match")
+
     def test_registration(self):
         self.get('/accounts/register/')
 
@@ -42,6 +67,8 @@ class FullRegistrationUITest(NycTreesSeleniumTestCase):
         self.wait_for_textbox_then_type('[name="email"]', self.email)
         self.wait_for_textbox_then_type('[name="password1"]', self.password)
         self.wait_for_textbox_then_type('[name="password2"]', self.password)
+        self.click('[name="tos"]')
+        self.click('[name="age_over_13"]')
 
         self.click('form input[type="submit"]')
 
@@ -58,6 +85,41 @@ class FullRegistrationUITest(NycTreesSeleniumTestCase):
         self.assertEqual(self.username, user.username)
         self.assertEqual(self.email, user.email)
         self.assertTrue(user.is_active)
+        self.assertFalse(user.is_minor)
+
+        # Clear the test inbox
+        mail.outbox = []
+
+    def test_registration_as_minor(self):
+        self.get('/accounts/register/')
+
+        username = 'test'
+        email = 'test@aol.com'
+
+        self.wait_for_textbox_then_type('[name="username"]', username)
+        self.wait_for_textbox_then_type('[name="email"]', email)
+        self.wait_for_textbox_then_type('[name="password1"]', 'banana')
+        self.wait_for_textbox_then_type('[name="password2"]', 'banana')
+        self.click('[name="tos"]')
+
+        # Omit clicking "I am over 13 years old" checkbox.
+
+        self.click('form input[type="submit"]')
+
+        self.wait_for_text("check your email")
+
+        self.assertEqual(mail.outbox[0].subject,
+                         "Account activation on NYC TreesCount!")
+
+        activation_link = self._get_link_from_email(mail.outbox[0])
+
+        self.sel.get(activation_link)
+
+        user = User.objects.get(username=username)
+        self.assertEqual(username, user.username)
+        self.assertEqual(email, user.email)
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_minor)
 
         # Clear the test inbox
         mail.outbox = []
