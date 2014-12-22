1a19105a3dbfbf67a35726b356af9b06ea25f680
Kevin DeLoach <kdeloach@azavea.com>
2014-12-22 16:18:23 -0500

    Add Follow/Unfollow group button

--- src/nyc_trees/js/src/group_detail.js
+++ src/nyc_trees/js/src/group_detail.js
@@ -6,6 +6,9 @@ var $ = require('jquery'),
         eventInfoSubmenuToggle: '[data-class="group-detail-event-info-submenu-toggle"]'
     };
 
+// Needed for Follow button event handlers.
+require('./followGroupButton');
+
 $(dom.eventInfoSubmenuToggle).on('click', function (e) {
     e.preventDefault();
     $(e.target).siblings(dom.eventInfoSubmenu).toggle();
--- src/nyc_trees/js/src/group_list.js
+++ src/nyc_trees/js/src/group_list.js
@@ -1,5 +1,4 @@
 "use strict";
 
-var $ = require('jquery');
-
-// TODO: implement
+// Needed for Follow button event handlers.
+require('./followGroupButton');
