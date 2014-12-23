10118fc26784a4db89d195164f50bf9d853b29d6
Kevin DeLoach <kdeloach@azavea.com>
2014-12-23 11:44:50 -0500

    Add tests for Follow/Unfollow group button

--- src/nyc_trees/apps/users/uitests.py
+++ src/nyc_trees/apps/users/uitests.py
@@ -10,9 +10,9 @@ from libs.ui_test_helpers import NycTreesSeleniumTestCase
 from apps.core.models import User, Group
 
 
-class GroupUITest(NycTreesSeleniumTestCase):
+class BaseGroupUITest(NycTreesSeleniumTestCase):
     def setUp(self):
-        super(GroupUITest, self).setUp()
+        super(BaseGroupUITest, self).setUp()
         self.public_user = User.objects.create(
             username='public_user',
             email='public_user@foo.com'
@@ -46,7 +46,11 @@ class GroupUITest(NycTreesSeleniumTestCase):
         )
 
     @property
-    def group_url(self):
+    def group_list_url(self):
+        return reverse('group_list_page')
+
+    @property
+    def group_detail_url(self):
         return reverse('group_detail', kwargs={
             'group_slug': self.group.slug
         })
@@ -57,8 +61,10 @@ class GroupUITest(NycTreesSeleniumTestCase):
             'group_slug': self.group.slug
         })
 
+
+class GroupUITest(BaseGroupUITest):
     def test_anonymous_user_can_see_group_page(self):
-        self.get(self.group_url)
+        self.get(self.group_detail_url)
         self.wait_for_text(self.group.name)
 
     def test_anonymous_user_redirected_when_getting_group_edit_page(self):
@@ -79,3 +85,37 @@ class GroupUITest(NycTreesSeleniumTestCase):
         self.login(self.census_admin_user.username)
         self.get(self.group_edit_url)
         self.wait_for_element('textarea[name="description"]')
+
+
+class FollowGroupUITest(BaseGroupUITest):
+    def _click_follow(self):
+        self.wait_for_text('Follow')
+        self.click('.btn-follow')
+
+    def _click_unfollow(self):
+        self.wait_for_text('Following')
+        self.click('.btn-unfollow')
+
+    def test_anonymous_user_follow_redirect_on_group_list_page(self):
+        self.get(self.group_list_url)
+        self._click_follow()
+        self.wait_for_text('Password:')
+
+    def test_anonymous_user_follow_redirect_on_group_detail_page(self):
+        self.get(self.group_detail_url)
+        self._click_follow()
+        self.wait_for_text('Password:')
+
+    def test_follow_on_group_list_page(self):
+        self.login(self.public_user.username)
+        self.get(self.group_list_url)
+        self._click_follow()
+        self._click_unfollow()
+        self._click_follow()
+
+    def test_follow_on_group_detail_page(self):
+        self.login(self.public_user.username)
+        self.get(self.group_detail_url)
+        self._click_follow()
+        self._click_unfollow()
+        self._click_follow()
