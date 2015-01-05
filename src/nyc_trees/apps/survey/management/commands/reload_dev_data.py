17e4823184d822c2e917d929497b5a6ff7972040
Michael Maurizi <mmaurizi@azavea.com>
2015-01-05 11:54:42 -0500

    Add management command to reload survey models

---
+++ src/nyc_trees/apps/survey/management/commands/reload_dev_data.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from apps.survey.models import (Tree, Survey, Blockface, Territory,
+                                BlockfaceReservation)
+
+
+class Command(BaseCommand):
+    """
+    Create a new instance with a single editing role.
+    """
+
+    option_list = BaseCommand.option_list
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        self.stdout.write('Deleting all existing survey and blockface data')
+
+        Tree.objects.all().delete()
+        Survey.objects.all().delete()
+        Territory.objects.all().delete()
+        BlockfaceReservation.objects.all().delete()
+        Blockface.objects.all().delete()
+
+        self.stdout.write('Importing blockface fixture')
+
+        call_command(
+            'loaddata', '/opt/app/apps/survey/fixtures/blockface.json')
