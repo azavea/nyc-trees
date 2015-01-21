620813389cb44d55c7693ddb4232d1263d5f9c77
Justin Walgran <jwalgran@azavea.com>
2015-01-21 16:17:47 -0700

    Add SES/boto based email backend for prod
    Using boto on a properly provisioned EC2 keeps the application out of
    the business of managing email credentials.
    
    All boto interaction is isolated in the ``BotoMailer`` class, which has
    no dependencies on Django and can be tested in isolation.
    
    This backend is configured for production only because it requires
    EC2 metadata services that aren't available on vagrant VMs.

---
+++ src/nyc_trees/libs/boto_mailer.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+
+import boto.ses
+
+from email.mime.text import MIMEText
+
+
+class BotoMailer():
+
+    def __init__(self, aws_region):
+        self.aws_region = aws_region
+
+    def send_plain_text_message(self, text, subject, from_email, to):
+        message = MIMEText(text)
+        message['Subject'] = subject
+        message['From'] = from_email
+        message['To'] = to
+        self.send_message_bytes(message.as_string(), from_email, to)
+
+    def send_message_bytes(self, message_bytes, from_email, to):
+        return self._connection().send_raw_email(message_bytes, from_email, to)
+
+    def get_remaining_message_quota(self):
+        response = self._connection().get_send_quota()
+        quota = response['GetSendQuotaResponse']['GetSendQuotaResult']
+        limit = int(float(quota('Max24HourSend')))
+        sent_count = int(float(quota('SentLast24Hours')))
+        return limit - sent_count
+
+    def _connection(self):
+        return boto.ses.connect_to_region(self.aws_region)
