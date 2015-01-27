4a4f5946008754e5abca5f1ff8a77d5832d8166b
Steve Lamb <slamb@azavea.com>
2015-01-27 14:57:19 -0500

    Add support for training flatpage subpages

--- src/nyc_trees/js/src/trainingFlatpage.js
+++ src/nyc_trees/js/src/trainingFlatpage.js
@@ -1,4 +1,32 @@
 "use strict";
 
-var $ = require('jquery');
+var $ = require('jquery'),
+    dom = {container: '[data-class="training-flatpage-container"]',
+           trainingSubpage: 'div',
+           nextButton: '[data-class="training-flatpage-subpage-next"]'},
+    $container = $(dom.container),
+    $subpages = $container.find(dom.trainingSubpage),
+    pageCount = $subpages.length,
+    $nextButton = $(dom.nextButton),
+    currentPage = 0;
 
+function showPageOrStep() {
+    if (currentPage < pageCount) {
+        $subpages.hide();
+        $($subpages[currentPage]).show();
+        currentPage++;
+    } else {
+        window.location.href = $nextButton.attr('data-href');
+    }
+}
+
+// container starts hidden so that subpages won't appear before
+// this js has a chance to hide them
+$subpages.hide();
+$container.show();
+
+$nextButton.on('click', showPageOrStep);
+
+if (pageCount > 0) {
+    showPageOrStep();
+}
