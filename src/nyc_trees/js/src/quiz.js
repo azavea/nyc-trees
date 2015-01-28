5c24efc506904fe9e7f736b3ae0444978246637f
Kevin DeLoach <kdeloach@azavea.com>
2015-01-28 15:39:08 -0500

    Merge quizProgress into quiz module
    The quizProgress module wasn't doing much since most of the
    saving/loading logic is now handled by the storage module.

--- src/nyc_trees/js/src/quiz.js
+++ src/nyc_trees/js/src/quiz.js
@@ -1,7 +1,8 @@
 "use strict";
 
 var $ = require('jquery'),
-    QuizProgress = require('./quizProgress');
+    util = require('./lib/util'),
+    Storage = require('./lib/storage');
 
 var dom = {
     next: '.quiz .next',
@@ -9,11 +10,36 @@ var dom = {
     submit: '.quiz .submit',
     warning: '.quiz .active .alert',
     quizSlug: '.quiz[data-quiz-slug]',
-    quizInputs: ".quiz input[type!='submit']"
+    quizInputs: ".quiz input[type!='submit']",
+    selections: '.quiz input:checked',
+    activeQuestion: '.quiz .active.question'
 };
 
 var slug = $(dom.quizSlug).data('quiz-slug'),
-    progress = new QuizProgress(slug);
+    progress = new Storage({
+        key: 'quiz-progress-' + slug,
+        getState: function() {
+            var formData = {};
+            $(dom.selections).each(function() {
+                formData[$(this).attr('name')] = $(this).val();
+            });
+            return {
+                form: formData,
+                question: $(dom.activeQuestion).data('question')
+            };
+        },
+        validate: function(state) {
+            if (util.isNullOrUndefined(state)) {
+                throw new Error('Unable to parse serialized state');
+            }
+            if (util.isNullOrUndefined(state.form)) {
+                throw new Error('Expected `state.form` to exist');
+            }
+            if (!$.isNumeric(state.question)) {
+                throw new Error('Expected `state.question` to be numeric');
+            }
+        }
+    });
 
 function next(e) {
     /*jshint validthis:true */
