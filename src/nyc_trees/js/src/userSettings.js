c897e688bc6c3e763bd8ab42b906a7bd555b6189
Steve Lamb <slamb@azavea.com>
2015-02-11 21:19:59 -0500

    Enable tab hashing for user settings page

--- src/nyc_trees/js/src/userSettings.js
+++ src/nyc_trees/js/src/userSettings.js
@@ -11,4 +11,6 @@ $(dom.form).on('submit', function () {
     privacySettings.prepareForSave();
 });
 
+require('./lib/tabhash/')('#user-settings-tabs');
+
 // Note that "Cancel" is handled by reloading the page so we don't need to do anything
