4db7042c1f56881ca889c970ee7d8adb1a8d5f8d
Rick Mohr <rmohr@azavea.com>
2015-02-20 10:45:25 -0500

    Fix lint

--- src/nyc_trees/nyc_trees/tests.py
+++ src/nyc_trees/nyc_trees/tests.py
@@ -3,8 +3,6 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 
-import json
-
 from django.contrib.gis.geos import LineString, MultiLineString
 from django.test import TestCase
 
