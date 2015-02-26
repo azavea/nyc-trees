5b6a410f6c928ad8efac3b67b4c4ea40efb3f003
Michael Maurizi <mmaurizi@azavea.com>
2015-02-26 11:05:03 -0500

    Add tree form JS interactivity
    Adds a bunch of small event handlers for showing/hiding or checking/unchecking
    fields when appropriate.
    
    Also brings in Handlebars.js, for creating programmatically generated DOM ids
    when adding new tree forms.

--- src/nyc_trees/js/src/surveyPage.js
+++ src/nyc_trees/js/src/surveyPage.js
@@ -2,6 +2,7 @@
 
 var $ = require('jquery'),
     L = require('leaflet'),
+    Handlebars = require('handlebars'),
     mapModule = require('./map'),
     mapUtil = require('./lib/mapUtil'),
     SelectableBlockfaceLayer = require('./lib/SelectableBlockfaceLayer');
@@ -14,8 +15,18 @@ var dom = {
     selectSide: '#select-side',
     leftRightButtons: '#btn-left, #btn-right',
     btnGroupNext: '#btn-group-next',
-    btnNext: '#btn-next'
+    btnNext: '#btn-next',
+
+    pageContainer: '#pages',
+    surveyPage: '#survey',
+    treeFormTemplate: '#tree-form-template',
+    treeFormcontainer: '#tree-form-container',
+
+    addTree: '#another-tree',
+    submitSurvey: '#submit-survey'
 };
+
+var formTemplate = Handlebars.compile($(dom.treeFormTemplate).html());
 
 var blockfaceId = mapUtil.getBlockfaceIdFromUrl(),
     blockfaceMap = mapModule.create({
@@ -95,6 +106,127 @@ function showPage(selector) {
     pages.filter(selector).removeClass('hidden');
 }
 
+// There is attribute for requiring "one or more" of a group of checkboxes to
+// be selected, so we have to handle it ourselves.
+$(dom.treeFormcontainer).on('change', 'input[name="problems"]', function () {
+    var $form = $(this).closest('form');
+    var $checkboxes = $form.find('input[name="problems"]');
+
+    if ($checkboxes.filter(':checked').length === 0) {
+        $checkboxes.prop('required', true);
+    } else {
+        $checkboxes.prop('required', false);
+    }
+});
+
+// When "No problems" is selected, we should clear all of the other options
+$(dom.treeFormcontainer).on('change', 'input[name="problems"][value="None"]', function () {
+    var $form = $(this).closest('form');
+    var $checkboxes = $form.find('input[name="problems"]').not(this);
+
+    if ($(this).is(':checked')) {
+        $checkboxes.prop('checked', false);
+    }
+});
+
+// When other problems are checked, "No problems" should be cleared
+$(dom.treeFormcontainer).on('change', 'input[name="problems"]:not([value="None"])', function () {
+    var $form = $(this).closest('form');
+    var $noProblems = $form.find('input[name="problems"][value="None"]');
+
+    if ($(this).is(':checked')) {
+        $noProblems.prop('checked', false);
+    }
+});
+
+// When "Status" is changed, we should show/hide the appropriate sections
+$(dom.treeFormcontainer).on('change', 'input[name="status"]', function () {
+    var $form = $(this).closest('form');
+    var $sections = $form.find('[data-status]');
+    var currentStatus = $form.find('input[name="status"]:checked').val();
+
+    $sections.addClass('hidden');
+    $sections.filter('[data-status="' + currentStatus + '"]').removeClass('hidden');
+});
+
+// Helper for checking the validity of a form
+function checkFormValidity() {
+    var $forms = $(dom.treeFormcontainer).find('form');
+    var valid = true;
+
+    // For each form element
+    $forms.find('input, select, textarea').each(function(i, el) {
+        if ($(el).is(':visible') && !el.validity.valid) {
+            valid = false;
+            $(el).focus();
+            if (el.select) {
+                el.select();
+            }
+
+            // "submit" the form.  This will trigger the builtin browser validation messages.
+            // Our submit handler will prevent this from actually submitting
+            $(el).closest('form').find('[data-class="fake-submit"]').click();
+
+            return false;
+        }
+    });
+
+    return valid;
+}
+
+// We need to submit the form to see the error bubbles, but we don't want to
+// actually send any data.
+$(dom.treeFormcontainer).on('submit', 'form', function(e) {
+    e.preventDefault();
+});
+
+
 $(dom.btnNext).click(function(e) {
     showPage(dom.surveyPage);
+});
+
+$(dom.addTree).click(function (){
+    if (checkFormValidity()) {
+        var treeNumber = $(dom.treeFormcontainer).find('[data-class="tree-form"]').length + 1;
+
+        $(dom.treeFormcontainer).append(formTemplate({tree_number: treeNumber}));
+    }
+});
+
+$(dom.submitSurvey).on('click', function() {
+    // TODO: replace this with data from the form(s)
+    var data = {
+        survey: {
+            blockface_id: blockfaceId,
+            is_flagged: false,
+            has_trees: true,
+            is_mapped_in_blockface_polyline_direction: true,
+            is_left_side: true
+        },
+        trees: [
+            {
+                circumference: 10,
+                distance_to_tree: 100,
+                curb_location: 'OnCurb',
+                status: 'Alive',
+                species_certainty: 'Yes',
+                health: 'Good',
+                stewardship: 'None',
+                guards: 'None',
+                sidewalk_damage: 'NoDamage',
+                problems: ['Stones', 'Sneakers']
+            }
+        ]
+    };
+    $.ajax('/survey/', {
+            type: 'POST',
+            dataType: 'json',
+            data: JSON.stringify(data)
+        })
+        .done(function(content) {
+            window.alert('Successfully saved a test survey');
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            window.alert(errorThrown);
+        });
 });
