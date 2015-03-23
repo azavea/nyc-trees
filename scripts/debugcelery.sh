d3dbfcb0c4b7eebab759c67de4cd83242d4ea76d
Michael Maurizi <mmaurizi@azavea.com>
2015-03-23 11:10:53 -0400

    Add autoreload option to debugcelery.sh script

--- scripts/debugcelery.sh
+++ scripts/debugcelery.sh
@@ -6,6 +6,6 @@ set -e
 
 STOP_SERVICE="(sudo service nyc-trees-celery stop || /bin/true)"
 CHANGE_DIR="cd /opt/app/"
-RUN_CELERY="envdir /etc/nyc-trees.d/env celery -A 'nyc_trees.celery:app' worker -l debug"
+RUN_CELERY="envdir /etc/nyc-trees.d/env celery -A 'nyc_trees.celery:app' worker --autoreload -l debug"
 
 vagrant ssh app -c "$STOP_SERVICE && $CHANGE_DIR && $RUN_CELERY"
