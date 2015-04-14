2f5f2112c4c6b97b76fd268d1e79537dac696f0e
Steve Lamb <slamb@azavea.com>
2015-04-14 16:27:46 -0400

    Add support for arbitrary user actions on marking

--- src/nyc_trees/apps/home/training/decorators.py
+++ src/nyc_trees/apps/home/training/decorators.py
@@ -18,7 +18,7 @@ def render_flatpage(url):
     return fn
 
 
-def mark_user(attr):
+def mark_user(attr, extra_block=None):
     def outer_decorator(view_fn):
         @wraps(view_fn)
         @transaction.atomic
@@ -28,8 +28,20 @@ def mark_user(attr):
             if user.is_authenticated() and not getattr(user, attr):
                 setattr(user, attr, True)
                 user.save()
+                should_trigger_extra_block = True
+            else:
+                should_trigger_extra_block = False
 
             ctx = view_fn(request, *args, **kwargs)
+
+            # in case the extra block does things that don't
+            # work well with database transactions (like email)
+            # postpone it to the end of the transaction block
+            # to avoid cases in which an email is sent but the
+            # transaction is rolled back due to a later exception
+            if extra_block and should_trigger_extra_block:
+                extra_block(user)
+
             return ctx
         return wrapper
     return outer_decorator
