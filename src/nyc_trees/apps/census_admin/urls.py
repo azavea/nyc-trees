79fe48965cb5f55380a259781d95bf094cf191c3
RickMohr <rmohr@azavea.com>
2015-04-16 11:12:36 -0400

    Census admin page for uploading group polygons GeoJSON file
    * If any group names are unknown or there is a syntax error, update nothing and report error info
    * Otherwise replace the `group.border` multi-polygon for each group present in the upload, and show a summary of changes
    
    File format based on example from Parks (simplified).
    Note that this is not valid GeoJSON (lacking `type` attributes), but we can easily parse the parts we need.
    ```json
    {
        "features": [
            {
                "geometry": {
                    "rings": [
                        [
                            [-74.00433784505003, 40.66953878784512],
                            [-74.00321110794214, 40.67192229492897],
                            [-74.00289344462487, 40.67259424901714],
                            [-74.00433784505003, 40.66953878784512]
                        ]
                    ]
                },
                "attributes": {
                    "GroupName": "Midnight Mappers",
                }
            },
            {
                "geometry": {
                    "rings": [
                        [
                            [-73.90236842648156, 40.88726229418421],
                            [-73.90050742517315, 40.88673329379011],
                            [-73.89791910153626, 40.886186228573074],
                            [-73.90236842648156, 40.88726229418421],
                        ]
                    ]
                },
                "attributes": {
                    "GroupName": "Fledglings",
                }
            }
        ]
    }
    
    ```
    
    Fixes #1134

--- src/nyc_trees/apps/census_admin/urls.py
+++ src/nyc_trees/apps/census_admin/urls.py
@@ -5,17 +5,12 @@ from __future__ import division
 
 from django.conf.urls import patterns, url
 
-from django_tinsel.decorators import route
-
-from apps.core.decorators import census_admin_do
-from apps.census_admin.views import (start_admin_users_job,
-                                     start_admin_surveys_job, admin_edits_page,
-                                     admin_edits_partial,
-                                     admin_training_material_list,
-                                     admin_training_material)
+from apps.census_admin import routes as r
 
 
 # These URLs have the prefix 'census_admin/'
 urlpatterns = patterns(
     '',
+    url(r'^upload-group-polygons/$', r.upload_group_polygons,
+        name='upload_group_polygons'),
 )
--- src/nyc_trees/apps/census_admin/views.py
+++ src/nyc_trees/apps/census_admin/views.py
@@ -2,3 +2,74 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
+
+from collections import OrderedDict
+import json
+
+from django.contrib.gis.geos import MultiPolygon, Polygon
+from django.db import transaction
+
+from apps.core.models import Group
+
+
+def upload_group_polygons(request):
+    try:
+        file_obj = request.FILES['file']
+        file_obj.seek(0)
+        text = file_obj.read()
+    except Exception as e:
+        return {'error': "Error retrieving uploaded file: " + e.message}
+
+    group_data = OrderedDict([
+        (group.name, {
+            'group': group,
+            'previous_polygon_count':
+                len(group.border.coords) if group.border else 0,
+            'polygons': []})
+        for group in Group.objects.all()
+    ])
+
+    unknown_group_names = set()
+    try:
+        data = json.loads(text)
+        for feature in data['features']:
+            group_name = feature['attributes']['GroupName']
+            polygon_points = feature['geometry']['rings'][0]
+            if group_name not in group_data:
+                unknown_group_names.add(group_name)
+            else:
+                group_data[group_name]['polygons'].append(polygon_points)
+
+    except Exception as e:
+        return {'error': "Error in JSON structure: " + e.message}
+
+    if unknown_group_names:
+        names = list(unknown_group_names)
+        names.sort()
+        return {'unknown_group_names': names}
+
+    context = _update_group_polygons(group_data)
+    return context
+
+
+@transaction.atomic
+def _update_group_polygons(group_data):
+    for g in group_data.values():
+        group = g['group']
+        polygon_points = g['polygons']
+        polygon_count = len(polygon_points)
+        updated = polygon_count > 0
+        g['updated'] = updated
+        g['updated_polygon_count'] = \
+            polygon_count if updated else g['previous_polygon_count']
+
+        if updated:
+            try:
+                polygons = [Polygon(points) for points in polygon_points]
+                group.border = MultiPolygon(polygons)
+                group.clean_and_save()
+            except Exception as e:
+                return {'error': ("Error updating polygons for group %s: %s"
+                                  % (group.name, e.message))}
+
+    return {'updates': group_data.values()}
