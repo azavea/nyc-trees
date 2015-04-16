16eb5be733bec1411d3ca9313d79a465cf9699c8
RickMohr <rmohr@azavea.com>
2015-04-16 15:10:05 -0400

    Remove unused `census_admin` endpoints

--- src/nyc_trees/apps/census_admin/urls.py
+++ src/nyc_trees/apps/census_admin/urls.py
@@ -18,41 +18,4 @@ from apps.census_admin.views import (start_admin_users_job,
 # These URLs have the prefix 'census_admin/'
 urlpatterns = patterns(
     '',
-    url(r'^users-export/$',
-        census_admin_do(route(POST=start_admin_users_job)),
-        name='start_admin_users_job'),
-
-    url(r'^surveys-export/$',
-        census_admin_do(route(POST=start_admin_surveys_job)),
-        name='start_admin_surveys_job'),
-
-    url(r'^edits/$',
-        census_admin_do(route(GET=admin_edits_page)),
-        name='admin_edits_page'),
-
-    url(r'^edits-partial/$',
-        census_admin_do(route(GET=admin_edits_partial)),
-        name='admin_edits_partial'),
-
-    # TODO: Use Django flatpages for training POST/PUT endpoints
-
-    url(r'^training/$',
-        census_admin_do(route(GET=admin_training_material_list,
-                              # POST=add_training_material
-        )),
-        name='admin_training_material_list'),
-
-    url(r'^training/(?P<training_material_url_name>\w+)/$',
-        census_admin_do(route(GET=admin_training_material,
-                              # PUT=edit_training_material
-        )),
-        name='admin_training_material'),
-
-    # TODO: Verify that these are provided by the django admin, then delete
-    #
-    # url(r'^admin/users/(?P<username>\w+)/individual-mapper-request/$', census_admin(route(POST=confirm_individual_mapper, DELETE=deny_individual_mapper))),  # NOQA
-    # url(r'^admin/users/individual-mapper-requests/$', census_admin(route(GET=individual_mapper_requests_page))),  # NOQA
-    # url(r'^admin/group/(?P<group_name>\w+)/$', census_admin(route(GET=admin_group_detail, PUT=admin_update_group))),  # admin_update_group includes editing blockface reservations and setting admin  # NOQA
-    # url(r'^admin/group/(?P<group_name>\w+)/enable/$', census_admin(route(POST=enable_group))),  # NOQA
-    # url(r'^admin/group/(?P<group_name>\w+)/disable/$', census_admin(route(POST=disable_group))),  # NOQA
 )
--- src/nyc_trees/apps/census_admin/views.py
+++ src/nyc_trees/apps/census_admin/views.py
@@ -2,34 +2,3 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
-
-
-def start_admin_users_job(request):
-    # TODO: implement
-    # TODO: implement
-    pass
-
-
-def start_admin_surveys_job(request):
-    # TODO: implement
-    pass
-
-
-def admin_edits_page(request):
-    # TODO: implement
-    pass
-
-
-def admin_edits_partial(request):
-    # TODO: implement
-    pass
-
-
-def admin_training_material_list(request):
-    # TODO: implement
-    pass
-
-
-def admin_training_material(request):
-    # TODO: implement
-    pass
